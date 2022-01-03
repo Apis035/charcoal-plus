5b7f78279ef36bb54ae003c1a83f65a092a253bc
Cocoa <gd.apis035@gmail.com>
2022-01-03 21:48:19 +0700

    fix charcoal block cant be mined

--- src/main/java/net/apis035/charcoalplus/CharcoalPlus.java
+++ src/main/java/net/apis035/charcoalplus/CharcoalPlus.java
@@ -5,8 +5,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.block.Block;
-import net.minecraft.block.MapColor;
-import net.minecraft.block.Material;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -16,15 +15,15 @@ import net.minecraft.util.registry.Registry;
 public class CharcoalPlus implements ModInitializer {
 	public static final String MOD_ID = "charcoalplus";
 
-	public static Item COAL_PIECE;
-	public static Item CHARCOAL_PIECE;
-	public static Block CHARCOAL_BLOCK;
+	public static Item COAL_PIECE = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
+	public static Item CHARCOAL_PIECE = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
+	public static Block CHARCOAL_BLOCK = new Block(FabricBlockSettings.copyOf(Blocks.COAL_BLOCK));
 
 	@Override
 	public void onInitialize() {
-		COAL_PIECE = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "coal_piece"), new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
-		CHARCOAL_PIECE = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "charcoal_piece"), new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
-		CHARCOAL_BLOCK = Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "charcoal_block"), new Block(FabricBlockSettings.of(Material.STONE, MapColor.BLACK).requiresTool().strength(5.0f, 6.0f)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "coal_piece"), COAL_PIECE);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "charcoal_piece"), CHARCOAL_PIECE);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "charcoal_block"), CHARCOAL_BLOCK);
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "charcoal_block"), new BlockItem(CHARCOAL_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 
 		FuelRegistry registry = FuelRegistry.INSTANCE;
